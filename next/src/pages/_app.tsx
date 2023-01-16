79fad4d37c480b8d8493fcdb2405566abf15fa62
lamongabriel <gabriel-lamon@outlook.com>
2023-01-16 13:53:39 -0300

    removed forbidden meta tags from _document

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -2,7 +2,17 @@ import type { AppProps } from 'next/app';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+		<>
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+  );
 }
