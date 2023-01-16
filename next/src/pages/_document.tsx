760a76a250ca85ec126c05384a6fe05fa295e682
lamongabriel <gabriel-lamon@outlook.com>
2023-01-16 13:34:19 -0300

    meta tags and favicon

--- next/src/pages/_document.tsx
+++ next/src/pages/_document.tsx
@@ -4,12 +4,25 @@ import {
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+		<Html lang="en">
+			<Head>
+				<meta charSet="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='crossorigin' />
+				<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+				<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+				<link rel="shortcut icon" href="/images/favicon/favicon.ico"></link>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
   );
 }
