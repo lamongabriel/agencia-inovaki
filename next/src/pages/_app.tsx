327c02eeeb8b2535b0d5f02884b2056f1e768547
lamongabriel <gabriel-lamon@outlook.com>
2023-01-16 13:26:41 -0300

    eslint + editorconfig setup

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
