844f817e8c47de39955014fe8b2827f27fc51669
Gabriel Lamon <gabriel-lamon@outlook.com>
2023-02-09 14:31:18 -0300

    vtex page new layout

--- next/src/pages/servicos/vtex.tsx
+++ next/src/pages/servicos/vtex.tsx
@@ -1,12 +1,22 @@
 import Image from 'next/image';
 
-import { FaDollyFlatbed } from 'react-icons/fa';
+import {
+  FaClipboardList,
+  FaPencilAlt,
+  FaGlobeAmericas,
+  FaChartLine,
+  FaLock,
+  FaCity,
+  FaComments,
+  FaCheck,
+} from 'react-icons/fa';
 
 import Link from 'next/link';
 import BannerTop from '../../components/UI/Banner/BannerTop';
 import Layout from '../../components/UI/Layout';
 import Heading from '../../components/UI/Heading';
 import Text from '../../components/UI/Text';
+import SEO from '../../components/SEO';
 
 import logoVtex from '../../assets/images/logos/logo-vtex.svg';
 import bgPC from '../../assets/images/Servicos/bg-vtex-pc.png';
@@ -14,11 +24,21 @@ import bg from '../../assets/images/Projetos/Aquamar/bg-aquamar.png';
 import arrowDown from '../../assets/images/Servicos/arrow-down.png';
 import dottedPattern from '../../assets/images/dotted-pattern.png';
 
+import vtex1 from '../../assets/images/Servicos/Vtex/vtex-1.png';
+import vtex3 from '../../assets/images/Servicos/Vtex/vtex-3.png';
+
 import styles from '../../styles/pages/servicos/internal.module.scss';
+import Button from '../../components/UI/Button';
 
 export default function VTEX() {
   return (
 		<Layout>
+			<SEO
+				pageTitle='Inovaki | VTEX'
+				pageDescription='A VTEX é uma das melhores plataformas de e-commerce disponíveis no mercado. Não perca mais tempo, entre em contato conosco e veja como podemos ajudá-lo a ter sucesso no mundo digital.'
+				pageUrl='https://inovaki.vercel.app/servicos/vtex'
+				pageImage='/images/vtex.png'
+			/>
 			<BannerTop image={bg} auxiliaryText="A maior plataforma E-Commerce">
 				<strong>VTEX</strong> VTEX
 			</BannerTop>
@@ -31,120 +51,206 @@ export default function VTEX() {
 
 					<div className={`${styles.content} container mx-auto`}>
 						<div className={styles.experienceContent}>
-							<Image src={logoVtex} alt='Logo VTEX' />
-
-							<Heading small>
-								<strong>
-									A plataforma de comércio digital para grandes empresas! <br/>
-									 Proporcione a experiência que seus consumidores desejam com uma plataforma
-									completa e robusta.
-								</strong>
-							</Heading>
 						</div>
 
-						<Text>
-							Durante os anos de 2020 e 2021 nossa agência foi responsável por todo
-							suporte e manutenção do site da Aquamar na plataforma Vtex. Neste período
-							desenvolvemos melhorias na loja com implementações de APIs, criação de Landing
-							Pages e soluções administrativas.
-						</Text>
-						<Text>
-							Iniciamos nosso trabalho junto a marca no ano de 2020, antes da pandemia, onde
-							começamos prestando o suporte e manutenção em seu e-commerce utilizando a ferramenta
-							VTEX. Foi um grande desafio para nossa empresa e equipe, pois o projeto vinha de
-							outra empresa sem informações e tivemos que desbravar os códigos e os problemas
-							encontrados	durante nossas análises técnicas.
-						</Text>
+						<div className='row align-items-center'>
+							<div className='col-lg-5 text-end'>
+								<Image src={logoVtex} alt='Logo VTEX' />
+								<Heading small className='text-end'>
+									A plataforma de comércio digital para empresas!
+								</Heading>
+								<Text className='text-end'>
+									Você quer levar seu negócio digital ao próximo nível?
+									<strong> Nós somos especialistas na plataforma VTEX </strong>
+									e temos uma vasta experiência no desenvolvimento
+									de lojas virtuais para empresas de todos os setores,
+									incluindo gigantes do comércio brasileiro.
+									<br/>
+									<br/>
+									<div style={{
+									  border: '1px solid #e31c58',
+									  padding: '20px',
+									  lineHeight: '1.2em',
+									  textAlign: 'center',
+									}}>
+										Já desenvolvemos soluções incríveis para empresas como
+										<strong> Aquamar</strong>,
+										<strong> Hope Lingerie</strong>,
+										<strong> Fashly</strong> e
+										<strong> Custa Pouco Seduzir</strong>.
+									</div>
+								</Text>
+							</div>
+							<Image className='col-lg-5 offset-lg-2' src={vtex1} alt='VTEX'/>
+						</div>
+
+						<div className='row align-items-center my-4'>
+							<Image className='col-lg-5' src={vtex3} alt='VTEX'/>
+							<div className='col-lg-5 offset-lg-2'>
+								<Heading small className='text-start'>
+									O que fazemos?
+								</Heading>
+								<div style={{
+								  lineHeight: '3rem',
+								}}>
+									<div style={{
+									  display: 'flex',
+									  alignItems: 'center',
+									  justifyContent: 'start',
+									  gap: '1rem',
+									}}>
+										<FaCheck size={24} color='#e31c58' />
+										<p style={{ margin: 0 }}>Somos especiadivstas em VTEX IO</p>
+									</div>
+
+									<div>Desenvolvemos soluções de pagamento;</div>
+
+									<div>Customizamos layouts com sua marca;</div>
+
+									<div>Criamos Landing Pages que convertem;</div>
+
+									<div>Integramos outras plataformas na VTEX;</div>
+
+									<div>Prestamos suporte e manutenção de lojas;</div>
+
+									<div>Possuímos equipe multifuncional;</div>
+								</div>
+							</div>
+						</div>
+
+						<hr />
+							<div style={{
+							  margin: '60px auto',
+							  textAlign: 'center',
+							  width: '80%',
+							}}>
+								<p style={{
+								  fontSize: '40px',
+								  lineHeight: '1em',
+								}}>
+									Não perca mais tempo. Escolha a Inovaki e tenha a
+									<strong> certeza de que seu negócio estará nas
+									mãos de especialistas VTEX</strong>.
+								</p>
+								<Button bg='#e31c58' color='#fff' rightIcon={FaComments}>
+									Fale com nosso comercial
+								</Button>
+							</div>
+						<hr />
+						<br />
+
 						<div className='w-100 text-center'>
-							<Image className='w-100' src={bgPC} alt='PC Dashboard VTEX'/>
+							<Image className='w-50' src={bgPC} alt='PC Dashboard VTEX'/>
 						</div>
 					</div>
 				</section>
 				<section className={styles.projectSheet}>
 					<div className={styles.buttonPlatform}>
-						CONHEÇA A PLATAFORMA
+						O QUE É A VTEX?
+					</div>
+					<div className='container' style={{
+					  background: '#e31c58',
+					  color: '#fff',
+					  padding: '30px',
+					  borderRadius: '20px',
+					}}>
+						<p style={{ color: '#fff' }}>
+							Se você está procurando por uma solução completa para o
+							sucesso do seu negócio digital, então você precisa conhecer a plataforma
+							<strong> VTEX</strong>.
+							Ela é diferenciada por oferecer recursos incríveis, como
+							<strong> facilidade de uso, gestão centralizada,
+								customização e estabilidade</strong>,
+							tornando-a a escolha perfeita para
+							empresas que buscam <strong>excelência na gestão do e-commerce</strong>.
+						</p>
 					</div>
 					<div className={`${styles.projectSheetContent} container row row-cols-lg-3 row-cols-1`}>
+
 						<div className={styles.benefits}>
 							<div>
-								<FaDollyFlatbed color='white'/>
+								<FaClipboardList color='white'/>
 							</div>
 							<Heading small>
-								Motor de preços avançado
+								Gestão centralizada do seu negócio
 							</Heading>
 							<Text>
-								Gerencie suas estratégias de venda e use táticas
-								avançadas de preço, baseadas nas suas próprias regras de segmentação.
+								Com a plataforma, o lojista tem uma completa gestão do seu
+								negócio, incluindo a administração de produtos, ofertas,
+								gestão de estoque e controle de vendas.
 							</Text>
 						</div>
 						<div className={styles.benefits}>
 							<div>
-								<FaDollyFlatbed color='white'/>
+								<FaPencilAlt color='white'/>
 							</div>
 							<Heading small>
-								Motor de preços avançado
+								Personalize sua loja com a sua marca
 							</Heading>
 							<Text>
-								Gerencie suas estratégias de venda e use táticas
-								avançadas de preço, baseadas nas suas próprias regras de segmentação.
+								A VTEX é altamente flexível em relação a personalização,
+								permitindo que o lojista escolha como será sua loja virtual,
+								de acordo com suas necessidades e metas.
 							</Text>
 						</div>
 						<div className={styles.benefits}>
 							<div>
-								<FaDollyFlatbed color='white'/>
+								<FaGlobeAmericas color='white'/>
 							</div>
 							<Heading small>
-								Motor de preços avançado
+								Múltiplas línguas e moedas
 							</Heading>
 							<Text>
-								Gerencie suas estratégias de venda e use táticas
-								avançadas de preço, baseadas nas suas próprias regras de segmentação.
+								Expanda seus negócios para o mundo inteiro com lojas multilíngues
+								e gerencie moedas e formas de pagamento locais com tranquilidade.
 							</Text>
 						</div>
 						<div className={styles.benefits}>
 							<div>
-								<FaDollyFlatbed color='white'/>
+								<FaChartLine color='white'/>
 							</div>
 							<Heading small>
-								Motor de preços avançado
+								Estabilidade e escalabilidade
 							</Heading>
 							<Text>
-								Gerencie suas estratégias de venda e use táticas
-								avançadas de preço, baseadas nas suas próprias regras de segmentação.
+							Com a VTEX, você pode ter a tranquilidade de uma loja virtual
+							sempre funcional. Além disso, a escalabilidade da
+							plataforma garante seu negócio sempre no topo.
 							</Text>
 						</div>
 						<div className={styles.benefits}>
 							<div>
-								<FaDollyFlatbed color='white'/>
+								<FaLock color='white'/>
 							</div>
 							<Heading small>
-								Motor de preços avançado
+								Segurança nas operações
 							</Heading>
 							<Text>
-								Gerencie suas estratégias de venda e use táticas
-								avançadas de preço, baseadas nas suas próprias regras de segmentação.
+								Escolha a plataforma e tenha uma experiência de
+								compra e venda sempre segura e tranquila,
+								tanto para o lojista quanto para o comprador.
 							</Text>
 						</div>
 						<div className={styles.benefits}>
 							<div>
-								<FaDollyFlatbed color='white'/>
+								<FaCity color='white'/>
 							</div>
 							<Heading small>
-								Motor de preços avançado
+								Plataforma omnichannel
 							</Heading>
 							<Text>
-								Gerencie suas estratégias de venda e use táticas
-								avançadas de preço, baseadas nas suas próprias regras de segmentação.
+								Com a VTEX, você pode integrar diversos canais de venda,
+								desde marketplaces até redes sociais, tornando a venda mais fácil e eficiente.
 							</Text>
 						</div>
 					</div>
 
 					<div className={`${styles.buttonsMore} row container mx-auto`}>
-						<Link href='https://vtex.com/br-pt/' className={`${styles.buttonPlatform} col-12 col-md-4`}>
-							SAIBA MAIS
-						</Link>
-						<Link href='#' className={`${styles.buttonPlatform} col-12 col-md-4 offset-md-1`}>
+						<Link href='#' className={`${styles.buttonPlatform} col-12 col-md-4`}>
 							CONHEÇA NOSSOS PROJETOS
+						</Link>
+						<Link href='https://vtex.com/br-pt/' className={`${styles.buttonPlatform} col-12 col-md-4 offset-md-1`}>
+							ACESSE A VTEX
 						</Link>
 					</div>
 
